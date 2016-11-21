3a3209d76a350c5e0a83c675b56707556a41f213
unknown <Administrator@VT2TYKFH3ZBKAEQ.(none)>
2016-11-21 22:38:04 +0800

    Initial commit

---
+++ app/src/main/java/com/yangll/bishe/happyweather/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package com.yangll.bishe.happyweather.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.yangll.bishe.happyweather.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
